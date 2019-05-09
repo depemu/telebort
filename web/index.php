591244012fcbc4b37c41c3dbad35345d08df2309
Muhar Dwi Purwanto <k@muhar.us>
2019-05-09 18:29:32 +0700

    ADD: main index and functions

--- web/index.php
+++ web/index.php
@@ -1,3 +1,13 @@
 <?php
 
 require('../vendor/autoload.php');
+
+$botUsername = '@telebortbot';
+$botToken = '750295559:AAHFZuX98qhYlCwroJppnMmbQ1w4EfJ4vMs';
+
+include dirname(__FILE__) . '/private/functions.php';
+include dirname(__FILE__) . '/private/telegram.php';
+
+$entityBody = file_get_contents('php://input');
+$receivedMessage = json_decode($entityBody, true);
+processMessage($receivedMessage);
